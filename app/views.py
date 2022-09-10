457e9f88bd6a456918d4ccf83d5c0209842e5785
Abhishek-003 <skabhishekgz6@gmail.com>
2022-09-11 00:34:13 +0530

    implemented the user registration functionality

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from unicodedata import category
 from django.shortcuts import render
 from .models import Customer, Product, OrderPlaced, Cart
+from .forms import CustomerRegistrationForm
 from django.views import View
 
 class ProductView(View):
@@ -37,14 +38,33 @@ def orders(request):
 def change_password(request):
  return render(request, 'app/changepassword.html')
 
-def mobile(request):
- return render(request, 'app/mobile.html')
+def mobile(request, data=None):
+    if data is None:
+        mobiles = Product.objects.filter(category='M')
+    elif data == 'below':
+        mobiles = Product.objects.filter(category='M').filter(discounted_price__lt=10000)
+    elif data == 'above':
+        mobiles = Product.objects.filter(category='M').filter(discounted_price__gt=10000)
+    else:
+        mobiles = Product.objects.filter(category='M').filter(brand=data)
+
+    return render(request, 'app/mobile.html', {'mobiles':mobiles})
 
 def login(request):
  return render(request, 'app/login.html')
 
-def customerregistration(request):
- return render(request, 'app/customerregistration.html')
+
+class CustomerRegistrationView(View):
+    def get(self, request):
+        form = CustomerRegistrationForm()
+        return render(request, 'app/customerregistration.html', {'form':form})
+
+    def post(self, request):
+        form = CustomerRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return render(request, 'app/customerregistration.html', {'form':form})
+
 
 def checkout(request):
  return render(request, 'app/checkout.html')
