3dbcb8ff437af3c7124d97e372c775c43498c63e
Abhishek-003 <skabhishekgz6@gmail.com>
2022-09-14 01:34:27 +0530

    Added Login and Logout Functionality

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,8 @@
 from socket import fromshare
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UsernameField
 from django.contrib.auth.models import User
+from django.utils.translation import gettext, gettext_lazy as _
 
 class CustomerRegistrationForm(UserCreationForm):
     password1 = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class':'form-control'}))
@@ -16,3 +17,9 @@ class CustomerRegistrationForm(UserCreationForm):
             'first_name': forms.TextInput(attrs={'class':'form-control'}),
             'last_name': forms.TextInput(attrs={'class':'form-control'})
         }
+
+
+class LoginForm(AuthenticationForm):
+    username = UsernameField(widget=forms.TextInput(attrs={'autofocus':True, 'class':'form-control'}))
+    password = forms.CharField(label=_('Password'), strip=False, widget=forms.PasswordInput(attrs={'autocomplete':'current-password', 'class':'form-control'}))
+
--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from app import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+from .forms import LoginForm
 
 
 urlpatterns = [
@@ -15,7 +17,8 @@ urlpatterns = [
     path('changepassword/', views.change_password, name='changepassword'),
     path('mobile/', views.mobile, name='mobile'),
     path('mobile/<slug:data>', views.mobile, name='mobiledata'),
-    path('login/', views.login, name='login'),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='app/login.html', authentication_form=LoginForm), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
     path('registration/', views.CustomerRegistrationView.as_view(), name='customerregistration'),
     path('checkout/', views.checkout, name='checkout'),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
