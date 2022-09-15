447cb67abd41821fb18f182c13c004510c4e6427
Abhishek-003 <skabhishekgz6@gmail.com>
2022-09-15 22:58:58 +0530

    Modified Profile and Address Page

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from unicodedata import category
 from django.shortcuts import render
 from .models import Customer, Product, OrderPlaced, Cart
-from .forms import CustomerRegistrationForm
+from .forms import CustomerProfileForm, CustomerRegistrationForm
 from django.views import View
 from django.contrib import messages
 
@@ -27,11 +27,9 @@ def add_to_cart(request):
 def buy_now(request):
  return render(request, 'app/buynow.html')
 
-def profile(request):
- return render(request, 'app/profile.html')
-
 def address(request):
- return render(request, 'app/address.html')
+ add = Customer.objects.filter(user=request.user)
+ return render(request, 'app/address.html', {'add': add})
 
 def orders(request):
  return render(request, 'app/orders.html')
@@ -64,3 +62,26 @@ class CustomerRegistrationView(View):
 
 def checkout(request):
  return render(request, 'app/checkout.html')
+
+class ProfileView(View):
+    def get(self, request):
+        form = CustomerProfileForm()
+        return render(request, 'app/profile.html', {'form':form})
+
+    def post(self, request):
+        form = CustomerProfileForm(request.POST)
+        if form.is_valid():
+            user = request.user
+            name = form.cleaned_data['name']
+            state = form.cleaned_data['state']
+            locality = form.cleaned_data['locality']
+            city = form.cleaned_data['city']
+            zipcode = form.cleaned_data['zipcode']
+            reg = Customer(user=user, name=name, locality=locality, city=city, state=state, zipcode=zipcode)
+            reg.save()
+            messages.success(request, "Congratulations!! Profile Updated Successfully")
+        return render(request, 'app/profile.html', {'form':form})
+
+
+        
+
